539736a1dc915b2ab8e5a19e074e7d517d68029d
Erik Camacho <erik.camacho@interfactura.com>
2021-11-01 01:00:52 -0600

    CREATED model mine and validations

--- app/models/mine.rb
+++ app/models/mine.rb
@@ -1,3 +1,13 @@
 class Mine < ApplicationRecord
-    belongs_to :minefield
+  belongs_to :minefield
+
+  enum visibility_status: %i[hidden shown question_mark red_flag]
+  enum associated_item: %i[empty_space mine]
+
+  validates :x_position, presence: true 
+  validates :y_position, presence: true 
+
+  before_create do
+    self.visibility_status = "hidden"
+  end
 end
