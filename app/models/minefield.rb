55519bf323ec9a39be89ad234ca16cf927fd8148
Erik Camacho <erik.camacho@interfactura.com>
2021-11-01 01:00:27 -0600

    Validations for minefield model

--- app/models/minefield.rb
+++ app/models/minefield.rb
@@ -1,3 +1,13 @@
 class Minefield < ApplicationRecord
   has_many :mines
+
+  enum status: %i[created playing paused lost]
+  
+  validates :x_size, presence: true, numericality: { greater_than: 0 }
+  validates :y_size, presence: true, numericality: { greater_than: 0 }
+
+  before_create do
+    self.uuid = SecureRandom.uuid
+    self.status = 0
+  end
 end
