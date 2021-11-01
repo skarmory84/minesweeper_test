92a79d1974a0de6dcbc363631492f2036283a723
Erik Camacho <erik.camacho@interfactura.com>
2021-11-01 02:42:13 -0600

    ADDED initial values

--- app/models/mine.rb
+++ app/models/mine.rb
@@ -2,12 +2,13 @@ class Mine < ApplicationRecord
   belongs_to :minefield
 
   enum visibility_status: %i[hidden shown question_mark red_flag]
-  enum associated_item: %i[empty_space mine]
+  enum asociated_item: %i[empty_space mine]
 
   validates :x_position, presence: true 
   validates :y_position, presence: true 
 
   before_create do
     self.visibility_status = "hidden"
+    self.asociated_item = "empty_space"
   end
 end
